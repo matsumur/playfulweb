1294dc3eed43585e4ba237f4bdb5892ea4548a38
HOBIRON <yudainepnep@icloud.com>
2021-05-28 17:23:04 +0900

    feat: pagesにresearch.jsを追加

---
+++ pages/research.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Research() {
+  return (
+    <>
+      <h1>First Research</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
