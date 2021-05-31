f4ee28d1715f595d7bcfc7c4c4374f8ee2abbbfb
Kohei Matsumura <matsumur@mmmbp.local>
2021-05-31 19:47:09 +0900

    use Image instead of img

--- components/people.js
+++ components/people.js
@@ -1,5 +1,6 @@
 import { MailIcon, HomeIcon } from '@heroicons/react/solid'
 import { version } from 'react-dom'
+import Image from 'next/image'
 
 const faculties = [
   {
@@ -221,17 +222,27 @@ export function Faculty() {
           className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
         >
           <div className="flex-1 flex flex-col p-8">
-            <img className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" src={person.imageUrl} alt="" />
+              <Image
+                className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full"
+                width="400"
+                height="400"
+                src={person.imageUrl}
+                alt="Profile image"
+              />
             <h3 className="mt-6 text-gray-900 text-sm font-medium">{person.name}</h3>
             <dl className="mt-1 flex-grow flex flex-col justify-between">
               <dt className="sr-only">Title</dt>
               <dd className="text-gray-500 text-sm">{person.title}</dd>
+              {person.role && 
+              <>
               <dt className="sr-only">Role</dt>
               <dd className="mt-3">
                 <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
                   {person.role}
                 </span>
               </dd>
+              </>
+              }
             </dl>
           </div>
           <div>
