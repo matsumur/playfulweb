6a17c3b0ffb31e813cd2758ba686ccd79f55c04d
HOBIRON <yudainepnep@icloud.com>
2021-05-31 14:38:03 +0900

    fix: ファイル名をresearchからprojectに変更

---
+++ pages/projects.js
@@ -0,0 +1,12 @@
+import Page from "../components/page";
+import ResearchProject from "../components/researchProject";
+
+export default function Projects() {
+  return (
+    <>
+      <Page>
+        <ResearchProject></ResearchProject>
+      </Page>
+    </>
+  );
+}
