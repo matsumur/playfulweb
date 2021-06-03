9790c06f4b0e3d4cbcb57dcd9124919c148b8a9e
is0405 <Kawanojunitiro@icloud.com>
2021-06-03 21:07:08 +0900

    なおし

--- components/banner.js
+++ components/banner.js
@@ -39,18 +39,6 @@ export default function Banner() {
             </div>
           </div>
         </div>
-        <div>
-          <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-            <div className="py-16">
-              <About name="私たちについて">
-                <p>
-                  私たち、立命館大学情報理工学部プレイフルインタラクション研究室は、Human-Centred Designの手法を用いて実世界の問題をプレイフルに解決する集団です。研究者として、エンジニアとして、デザイナーとして、実践者として、アーティストとして、あるいはオタクとして、新しいデジタル技術を創造し、それがもたらす社会的価値や科学的価値を発見・発信します。これによって社会問題を解決することや未来の生活を楽しくすることを目指します。
-                </p>
-              </About>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        
   );
 }
