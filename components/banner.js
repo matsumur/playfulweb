d0f67bf78f4b68632cdd1c97219686b83c6516c6
Kohei Matsumura <matsumur@fc.ritsumei.ac.jp>
2021-06-05 20:30:46 +0900

    minor update

--- components/banner.js
+++ components/banner.js
@@ -1,7 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
-import Image from 'next/image'
-import About from './about'
-import Slider from './slider'
+import Image from "next/image";
+import About from "./about";
+import Slider from "./slider";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -9,36 +9,29 @@ function classNames(...classes) {
 
 export default function Banner() {
   return (
-        <div className="relative">
-          <div className="absolute inset-x-0 bottom-0 h-1/2" />
-          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
-              <div className="absolute inset-0">
-                {/*<Image
-                  className="h-full w-full object-cover"
-                  src="/images/top/home1.png"
-                  layout="fill"
-                  alt="People working on laptops"
-                />*/}
-                <Slider />
-              <div className="absolute inset-0 bg-gray-400 mix-blend-multiply z-10" />
-              </div>
-              <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8 z-10">
-                <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                  <Image
-                    className="block lg:hidden h-8 w-auto"
-                    src="/playful.svg"
-                    height="80"
-                    width="auto"
-                    alt="Playful Laboratory Logo"
-                  />
-                  <span className="block text-yellow-500">Playful</span>
-                  <span className="block text-white">Laboratory</span>
-                </h1>
-              </div>
-            </div>
+    <div className="relative">
+      <div className="absolute inset-x-0 bottom-0 h-1/2" />
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+          <div className="absolute inset-0">
+            <Slider />
+            <div className="absolute inset-0 bg-gray-400 mix-blend-multiply z-10" />
+          </div>
+          <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8 z-10">
+            <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+              <Image
+                className="block lg:hidden h-8 w-auto"
+                src="/playful.svg"
+                height="80"
+                width="auto"
+                alt="Playful Laboratory Logo"
+              />
+              <span className="block text-yellow-500">Playful</span>
+              <span className="block text-white">Laboratory</span>
+            </h1>
           </div>
         </div>
-        
+      </div>
+    </div>
   );
 }
--- components/slider.js
+++ components/slider.js
@@ -14,47 +14,55 @@ import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper/core";
 // install Swiper modules
 SwiperCore.use([Autoplay, Pagination, Navigation]);
 
+const swipeImages = [
+  {
+    id: 1,
+    src: "/images/top/home1.png",
+    alt: "People working on laptops",
+  },
+  {
+    id: 2,
+    src: "/images/top/home1.png",
+    alt: "People working on laptops",
+  },
+  {
+    id: 3,
+    src: "/images/top/home1.png",
+    alt: "People working on laptops",
+  },
+  {
+    id: 4,
+    src: "/images/top/home1.png",
+    alt: "People working on laptops",
+  },
+];
+
 export default function App() {
   return (
-    <>
-      <Swiper
-        spaceBetween={10}
-        centeredSlides={true}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        className="mySwiper"
-      >
-        <SwiperSlide>
+    <Swiper
+      spaceBetween={10}
+      centeredSlides={true}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      className="mySwiper"
+    >
+      {swipeImages.map((swipeImage) => (
+        <SwiperSlide key={swipeImage.id}>
           <Image
             className="h-full w-full object-cover"
-            src="/images/top/home1.png"
+            src={swipeImage.src}
             layout="fill"
-            alt="People working on laptops"
+            alt={swipeImage.alt}
+            href="/openlab2021"
           />
         </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            className="h-full w-full object-cover"
-            src="/images/top/home1.png"
-            layout="fill"
-            alt="People working on laptops"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            className="h-full w-full object-cover"
-            src="/images/top/home1.png"
-            layout="fill"
-            alt="People working on laptops"
-          />
-        </SwiperSlide>
-      </Swiper>
-    </>
+      ))}
+    </Swiper>
   );
 }
