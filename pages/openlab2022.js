c6c7310ac325662e876e7f6c0e1df95cbb17d535
Kohei Matstsumura <matsumur@fc.ritsumei.ac.jp>
2022-06-01 11:50:01 +0900

    openlab info update

--- pages/openlab2022.js
+++ pages/openlab2022.js
@@ -19,9 +19,11 @@ export default function Openlab() {
 
       <main className="flex flex-col justify-center w-full flex-1 px-20 py-5">
         <About name="研究室公開2022">
-          <p className="text-lg">2022年度の研究室公開は以下の日時に実施します。この時間内であればいつ来てくれても、いつお帰りいただいても構いません。</p>
-          <p className="text-lg">6月8日（水）16:20〜17:50 クリエーションコア3Fプレイフルインタラクション研究室</p>
-          <p className="text-lg">6月15日（水）16:20〜17:50 クリエーションコア3Fプレイフルインタラクション研究室</p>
+          <p className="text-lg">2022年度の研究室公開は以下の日時にクリエーションコア3Fプレイフルインタラクション研究室で実施します。この時間内であればいつ来てくれても、いつお帰りいただいても構いません。</p>
+          <ol className="text-lg">
+            <li>6月8日（水）16:20〜17:50</li>
+            <li>6月15日（水）16:20〜17:50</li>
+          </ol>
         </About>
 
         <Divider>本研究室の成り立ち</Divider>
