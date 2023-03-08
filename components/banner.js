a333d7543e6d61500a93cf670a3d13df957970de
is0405 <Junknhinamyzk@outlook.jp>
2023-03-08 16:43:53 +0900

    slider動かす

--- components/banner.js
+++ components/banner.js
@@ -13,23 +13,7 @@ export default function Banner() {
       <div className="absolute inset-x-0 bottom-0 h-1/2" />
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
-          <div className="absolute inset-0">
-            <Slider />
-            <div className="absolute inset-0 bg-gray-400 mix-blend-multiply z-10" />
-          </div>
-          <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8 z-10">
-            <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-              <Image
-                className="block lg:hidden h-8 w-auto"
-                src="/playful.svg"
-                height="80px"
-                width="300px"
-                alt="Playful Laboratory Logo"
-              />
-              <span className="block text-yellow-500">Playful</span>
-              <span className="block text-white">Laboratory</span>
-            </h1>
-          </div>
+          <Slider />
         </div>
       </div>
     </div>
--- components/slider.js
+++ components/slider.js
@@ -2,17 +2,15 @@ import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import Image from "next/image";
-// Import Swiper styles
+// import 'swiper/swiper-bundle.min.css' 
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css";
 import "swiper/components/navigation/navigation.min.css";
+// import required modules
+import SwiperCore, {Navigation, Autoplay, Pagination, Mousewheel, Keyboard } from "swiper/core";;
+SwiperCore.use([Navigation, Autoplay, Pagination, Mousewheel, Keyboard]);
 
-// import Swiper core and required modules
-import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper/core";
-
-// install Swiper modules
-SwiperCore.use([Autoplay, Pagination, Navigation]);
+import Image from "next/image";
 
 const swipeImages = [
   {
@@ -59,39 +57,62 @@ const swipeImages = [
   },
 ];
 
+function SlideImage(props) {
+  return (
+    <>
+      <div className="absolute inset-0">
+        <div className="absolute inset-0 bg-gray-400 mix-blend-multiply z-10" />
+        <Image
+          className="block h-full w-full"
+          src={props.swipeImage.src}
+          layout="fill"
+          alt={props.swipeImage.alt}
+        />
+      </div>
+      <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8 z-10">
+        <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+          <Image
+            className="block lg:hidden h-8 w-auto"
+            height="100px"
+            width="100px"
+            src="/playful.svg"
+            alt="Playful Laboratory Logo"
+          />
+          <span className="block text-yellow-500">Playful</span>
+          <span className="block text-white">Laboratory</span>
+        </h1>
+      </div>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <Swiper
+      slidesPerView={1}
       spaceBetween={10}
+      navigation={true}
+      mousewheel={true}
+      keyboard={true}
+      pagination={{
+        clickable: true,
+      }}
+      loop={true}
       centeredSlides={true}
       autoplay={{
-        delay: 3000,
+        delay: 2500,
         disableOnInteraction: false,
       }}
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      rewind="true"
+      scrollbar={{ draggable: true }}     
+      modules = {
+          [Navigation, Autoplay, Pagination, Mousewheel, Keyboard]
+      }
       className="mySwiper"
     >
+
       {swipeImages.map((swipeImage) => (
         <SwiperSlide key={swipeImage.id}>
-          {swipeImage.href ? (
-            <a href={swipeImage.href}>
-              <Image
-                className="h-full w-full object-cover"
-                src={swipeImage.src}
-                layout="fill"
-                alt={swipeImage.alt}
-              />
-            </a>
-          ) : (
-            <Image
-              className="h-full w-full object-cover"
-              src={swipeImage.src}
-              layout="fill"
-              alt={swipeImage.alt}
-            />
-          )}
+          <SlideImage swipeImage={swipeImage}/>
         </SwiperSlide>
       ))}
     </Swiper>
