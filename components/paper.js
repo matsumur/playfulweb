435b4618e8b60b0ef9d0e8819117f96165f5c0f8
Maki-Rin <M.Rintaro1011@icloud.com>
2024-04-09 21:50:50 +0900

    ✏️ 誤字の修正

--- components/paper.js
+++ components/paper.js
@@ -1,44 +1,55 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Papers(props) {
   const { papers, form, year } = props;
   return (
-      <div className="grid-cols-1">
-        {papers.map((paper) => (
-          <>{paper.form==form && paper.year==year && (
-              <div className="l-wrapper" key={paper.title}>
-                <article className="card border rounded-xl bg-gray-50">
-                  <header class="card-header">
-                    <div className="card-image">
-                      <img
-                        className = "img-small-size md:img-middle-size"
-                        src = { paper.imageURL ? "/project-images/" + paper.imageURL : "/project-images/noImage.png" }
-                        objectFit="cover"
-                      />
-                      <div class="flex-1">
-                        <h4 class="card-title margin-remove">【{paper.form}】</h4>
-                        <h4 class="card-title ">{paper.title}</h4>
-                        <ul class="card-meta card-nav">
-                          <p> {paper.publisher} <br />{paper.conforenceName}</p>
-                        </ul>
-                        <ul class="card-meta card-nav">
-                          {paper.document && (
-                            <a href={paper.document}>
-                              <p>DOI</p>
-                            </a>
-                          )}
-                        </ul>
-                      </div>
+    <div className='grid-cols-1'>
+      {papers.map((paper) => (
+        <>
+          {paper.form == form && paper.year == year && (
+            <div className='l-wrapper' key={paper.title}>
+              <article className='card border rounded-xl bg-gray-50'>
+                <header className='card-header'>
+                  <div className='card-image'>
+                    <img
+                      className='img-small-size md:img-middle-size'
+                      src={
+                        paper.imageURL
+                          ? '/project-images/' + paper.imageURL
+                          : '/project-images/noImage.png'
+                      }
+                      objectFit='cover'
+                    />
+                    <div className='flex-1'>
+                      <h4 className='card-title margin-remove'>
+                        【{paper.form}】
+                      </h4>
+                      <h4 className='card-title'>{paper.title}</h4>
+                      <ul className='card-meta card-nav'>
+                        <p>
+                          {' '}
+                          {paper.publisher} <br />
+                          {paper.conferenceName}
+                        </p>
+                      </ul>
+                      <ul className='card-meta card-nav'>
+                        {paper.document && (
+                          <a href={paper.document}>
+                            <p>DOI</p>
+                          </a>
+                        )}
+                      </ul>
                     </div>
-                  </header>
-                <div class="card-body hidden lg:inline-block ">
-                    <p>{paper.abstruct}</p>
+                  </div>
+                </header>
+                <div className='card-body hidden lg:inline-block '>
+                  <p>{paper.abstract}</p>
                 </div>
-            </article>
-        </div>
-        )}
-    </>
-    ))}
-</div>
-);
+              </article>
+            </div>
+          )}
+        </>
+      ))}
+    </div>
+  );
 }
