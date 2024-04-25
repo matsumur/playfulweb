314cf09440d846942351ac6083af601f5ae987c8
Maki-Rin <M.Rintaro1011@icloud.com>
2024-04-25 12:53:28 +0900

    💄 発表文献の表示方法を変更

--- pages/papers.js
+++ pages/papers.js
@@ -22,52 +22,52 @@ export default function Papers() {
           <p className='sm:text-base lg:text-lg'>発表した論文を紹介します。</p>
         </About>
         {/* <div id={'jornal2024'} />
-        <Divider>原著(2024年度)</Divider>
-        <Research papers={research} form={'原著'} year={2024} />
+        <Divider>論文誌(2024年)</Divider>
+        <Research papers={research} form={'論文'} year={2024} /> */}
 
         <div id={'oral2024'} />
-        <Divider>口頭発表(2024年度)</Divider>
-        <Research papers={research} form={'口頭発表'} year={2024} />
+        <Divider>査読付き発表(2024年)</Divider>
+        <Research papers={research} form={'査読付き発表'} year={2024} />
 
         <div id={'poster2024'} />
-        <Divider>ポスター発表(2024年度)</Divider>
-        <Research papers={research} form={'ポスター発表'} year={2024} /> */}
+        <Divider>査読なし発表(2024年)</Divider>
+        <Research papers={research} form={'査読なし発表'} year={2024} />
 
         {/* <div id={'jornal2023'} />
-        <Divider>原著(2023年度)</Divider>
-        <Research papers={research} form={'原著'} year={2023} /> */}
+        <Divider>論文誌(2023年)</Divider>
+        <Research papers={research} form={'論文'} year={2023} /> */}
 
         <div id={'oral2023'} />
-        <Divider>口頭発表(2023年度)</Divider>
-        <Research papers={research} form={'口頭発表'} year={2023} />
+        <Divider>査読付き発表(2023年)</Divider>
+        <Research papers={research} form={'査読付き発表'} year={2023} />
 
         <div id={'poster2023'} />
-        <Divider>ポスター発表(2023年度)</Divider>
-        <Research papers={research} form={'ポスター発表'} year={2023} />
+        <Divider>査読なし発表(2023年)</Divider>
+        <Research papers={research} form={'査読なし発表'} year={2023} />
 
-        <div id={'jornal2022'} />
-        <Divider>原著(2022年度)</Divider>
-        <Research papers={research} form={'原著'} year={2022} />
+        <div id={'jornal2023'} />
+        <Divider>論文誌(2023年)</Divider>
+        <Research papers={research} form={'論文'} year={2022} />
 
         <div id={'oral2022'} />
-        <Divider>口頭発表(2022年度)</Divider>
-        <Research papers={research} form={'口頭発表'} year={2022} />
+        <Divider>査読付き発表(2022年)</Divider>
+        <Research papers={research} form={'査読付き発表'} year={2022} />
 
         <div id={'poster2022'} />
-        <Divider>ポスター発表(2022年度)</Divider>
-        <Research papers={research} form={'ポスター発表'} year={2022} />
+        <Divider>査読なし発表(2022年)</Divider>
+        <Research papers={research} form={'査読なし発表'} year={2022} />
 
         <div id={'jornal2021'} />
-        <Divider>原著(2021年度)</Divider>
-        <Research papers={research} form={'原著'} year={2021} />
+        <Divider>論文誌(2021年)</Divider>
+        <Research papers={research} form={'論文'} year={2021} />
 
         <div id={'oral2021'} />
-        <Divider>口頭発表(2021年度)</Divider>
-        <Research papers={research} form={'口頭発表'} year={2021} />
+        <Divider>査読付き発表(2021年)</Divider>
+        <Research papers={research} form={'査読付き発表'} year={2021} />
 
-        <div id={'poster2021'} />
-        <Divider>ポスター発表(2021年度)</Divider>
-        <Research papers={research} form={'ポスター発表'} year={2021} />
+        {/* <div id={'poster2021'} />
+        <Divider>査読なし発表(2021年)</Divider>
+        <Research papers={research} form={'査読なし発表'} year={2021} /> */}
       </main>
     </Page>
   );
