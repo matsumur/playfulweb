a038a3bdc27be83c6e83ec65022d9941ba833636
Maki-Rin <M.Rintaro1011@icloud.com>
2024-05-28 09:36:52 +0900

    🚀 研究室公開ページの更新

--- pages/openlab2024.js
+++ pages/openlab2024.js
@@ -4,6 +4,7 @@ import About from '../components/about';
 import Faq from '../components/faq';
 import Divider from '../components/divider';
 import FaqAnswers from '../public/faq.json';
+import Graduates from '../public/graduate.json';
 import Link from 'next/link';
 
 export default function Openlab() {
@@ -22,14 +23,22 @@ export default function Openlab() {
         <About name='研究室公開2024'>
           <p className='sm:text-base lg:text-lg'>
             2024年度の研究室公開は
+            <a class='underline decoration-zink-500 decoration-2'>
+              6月5日（水）の5限（16:20〜）
+            </a>
+            と
+            <a class='underline decoration-zink-500 decoration-2'>
+              6月6日（木）の4限（14:40〜）
+            </a>
+            に
             <a class='underline decoration-pink-500 decoration-2'>
               H棟5階プレイフルインタラクション研究室（H522）
             </a>
-            で実施します。プレイフルインタラクション研究室は
+            で実施します。この時間であればいつ来てくれても、いつお帰りいただいても構いません。そもそもプレイフルインタラクション研究室は
             <a class='underline decoration-rose-500 decoration-2'>
               オープンマインド
             </a>
-            を標語としており、いつでもオープンですのでお気軽にお越しください。
+            を標語としており、いつでもオープンですので、そのほかの時間でもお気軽にお越しください。
           </p>
         </About>
 
@@ -46,6 +55,8 @@ export default function Openlab() {
           以下に例を紹介します。
         </p>
         <Lablife />
+        <Divider>研究室の生活（卒業生の場合）</Divider>
+        <LablifeGraduate />
         <Faq />
       </main>
     </Page>
@@ -91,6 +102,38 @@ export function Lablife() {
                   {person.master ? 'ある修士学生の場合' : 'ある学部学生の場合'}
                 </p>
               </a>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export function LablifeGraduate() {
+  return (
+    <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 sm:px-5 lg:px-10 mt-6'>
+      {Graduates.map((person) => (
+        <Link href={'graduate?id=' + person.id} key={person.id}>
+          <div className='relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex flex-col items-center space-y-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500'>
+            <div className='flex-shrink-0'>
+              {person.imageUrl ? (
+                <img
+                  className='w-10 h-10 rounded-full object-cover'
+                  src={'/images/' + person.imageUrl}
+                  alt=''
+                />
+              ) : (
+                <span className='inline-block w-10 h-10 rounded-full overflow-hidden bg-gray-100'>
+                  <svg
+                    className='h-full w-full text-gray-300'
+                    fill='currentColor'
+                    viewBox='0 0 24 24'
+                  >
+                    <path d='M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z' />
+                  </svg>
+                </span>
+              )}
             </div>
           </div>
         </Link>
