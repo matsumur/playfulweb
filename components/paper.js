77e1201daae0e66cc1d70ebda0bb70dc7ac470e8
Maki-Rin <M.Rintaro1011@icloud.com>
2025-05-13 09:37:25 +0900

    ⚡️ paper UIの調整

--- components/paper.js
+++ components/paper.js
@@ -8,44 +8,46 @@ export default function Papers(props) {
         <>
           {paper.form == form && paper.year == year && (
             <div className='l-wrapper' key={paper.title}>
-              <article className='card border rounded-xl bg-gray-50'>
-                <header className='card-header'>
-                  {/* <div className='card-image'>
-                    <img
-                      className='img-small-size md:img-middle-size'
-                      src={
-                        paper.imageURL
-                          ? '/project-images/' + paper.imageURL
-                          : '/project-images/noImage.png'
-                      }
-                      objectFit='cover'
-                    /> */}
-                  <div className='flex-1 px-5'>
-                    {/* <h4 className='card-title margin-remove'>
-                      【{paper.form}】
-                    </h4> */}
-                    <h4 className='card-title'>{paper.title}</h4>
-                    <ul className='card-meta card-nav'>
-                      <p>
-                        {' '}
-                        {paper.publisher} <br />
-                        {paper.conferenceName}
-                      </p>
-                    </ul>
-                    <ul className='card-meta card-nav'>
-                      {paper.document && (
-                        <a href={paper.document}>
-                          <p>DOI</p>
-                        </a>
-                      )}
-                    </ul>
-                  </div>
-                  {/* </div> */}
-                </header>
-                {/* <div className='card-body hidden lg:inline-block '>
-                  <p>{paper.abstract}</p>
-                </div> */}
-              </article>
+              {paper.document ? (
+                <a
+                  href={paper.document}
+                  className='block transition-all duration-200 hover:shadow-md relative group'
+                >
+                  <article className='card border rounded-xl bg-gray-50 hover:bg-gray-100 cursor-pointer transform hover:scale-[1.01] transition-all'>
+                    <header className='card-header'>
+                      <div className='flex-1 px-5'>
+                        <div className='flex items-center'>
+                          <h4 className='card-title flex-grow'>
+                            {paper.title}
+                          </h4>
+                        </div>
+                        <ul className='card-meta card-nav'>
+                          <p>
+                            {' '}
+                            {paper.publisher} <br />
+                            {paper.conferenceName}
+                          </p>
+                        </ul>
+                      </div>
+                    </header>
+                  </article>
+                </a>
+              ) : (
+                <article className='card border rounded-xl bg-gray-50'>
+                  <header className='card-header'>
+                    <div className='flex-1 px-5'>
+                      <h4 className='card-title'>{paper.title}</h4>
+                      <ul className='card-meta card-nav'>
+                        <p>
+                          {' '}
+                          {paper.publisher} <br />
+                          {paper.conferenceName}
+                        </p>
+                      </ul>
+                    </div>
+                  </header>
+                </article>
+              )}
             </div>
           )}
         </>
