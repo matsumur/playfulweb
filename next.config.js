2fd4dd0c380fa6dedac7d3ea01054cfdccaa1da2
Maki-Rin <M.Rintaro1011@icloud.com>
2025-07-03 12:25:40 +0900

    🔖 いつでも研究室公開に戻す

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    const years = [2024, 2023, 2022, 2021];
+    const years = [2025, 2024, 2023, 2022, 2021];
     return [
       ...years.map((year) => ({
         source: `/openlab${year}`,
         destination: '/openlab',
         permanent: false,
       })),
-      {
-        source: '/openlab',
-        destination: '/openlab2025',
-        permanent: false,
-      },
+      // {
+      //   source: '/openlab',
+      //   destination: '/openlab2025',
+      //   permanent: false,
+      // },
     ];
   },
 };
